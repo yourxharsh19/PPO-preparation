c62c8ba0f832f849a3a26a0153346f711874b465
Harshdeep Singh <harshdeep.2k6@gmail.com>
2026-08-19 00:52:02 +0530

    Time: 1 ms (99.76%), Space: 47.2 MB (77.03%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int curEnd = 0;
+        int farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+}
