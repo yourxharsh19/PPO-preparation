6e1f6eb891f901100196932d5991b63d76e2b7b1
Harshdeep Singh <harshdeep.2k6@gmail.com>
2026-09-08 20:44:06 +0530

    Time: 3 ms (26.21%), Space: 43 MB (41.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,16 +1,17 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> stringStack = new Stack<>();
-        String currentString = "";
-        int currentnum = 0;
-        for (int i = 0; i < s.length(); i++) {
+        Stack<Integer> countStack=new Stack<>();
+        Stack<String> stringStack=new Stack<>();
+         String currentString = "";
+         int currentnum=0;
+         for (int i = 0; i < s.length(); i++) {
             if (Character.isDigit(s.charAt(i))) {
                 int digit = s.charAt(i) - '0';
                  currentnum = currentnum * 10 + digit;
             } else if (Character.isLetter(s.charAt(i))) {
                 currentString += s.charAt(i);
-            } else if (s.charAt(i) == '[') {
+            }
+                else if (s.charAt(i) == '[') {
                 countStack.push(currentnum);
                 stringStack.push(currentString);
                 currentnum = 0;
@@ -21,7 +22,7 @@ class Solution {
                 String poppedstring=stringStack.pop();
                 currentString=poppedstring+currentString.repeat(poppednumber);
             }
-        }
-        return currentString;
+         }
+            return currentString;
     }
 }
